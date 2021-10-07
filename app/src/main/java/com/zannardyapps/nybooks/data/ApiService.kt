e27382b30a3b305cf23ffdeb9a7f926fc0ac74b4
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-10-07 16:41:03 -0300

    Implementação - Retrofit
    interface NewYorkTimesService que retorna uma @GET("lists.json"), ainda falta ser implementada.
    
    object ApiService que tem um método chamado initRetrofit()

---
+++ app/src/main/java/com/zannardyapps/nybooks/data/ApiService.kt
@@ -0,0 +1,18 @@
+package com.zannardyapps.nybooks.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiService {
+
+    private const val baseUrl: String = "https://api.nytimes.com/svc/books/v3"
+
+    private fun initRetrofit() = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val service: NewYorkTimesService
+    = initRetrofit().create(NewYorkTimesService::class.java)
+
+}
---
+++ app/src/main/java/com/zannardyapps/nybooks/data/NewYorkTimesService.kt
@@ -0,0 +1,12 @@
+package com.zannardyapps.nybooks.data
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewYorkTimesService {
+    /*
+    @GET("lists.json")
+    fun listRepos(): Call<List<>>
+     */
+
+}
